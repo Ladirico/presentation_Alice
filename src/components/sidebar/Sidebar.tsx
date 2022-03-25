030ac894ae32fc9a44953ea770080a998b0dea55
Alice-Aesys <alice.dirico@aesys.tech>
2022-03-25 12:24:11 +0100

    blocchiamoci va

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { SmartphoneView } from "../../App";
 import { SidebarWrapper } from "./Sidebar.styles";
 
 const Sidebar = () => {
+  //commento di prova
   const context = useContext(SmartphoneView);
   const showSidebar = context?.context.openSidebar;
   const smartView = context?.context.smartView;
